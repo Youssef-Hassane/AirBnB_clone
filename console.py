e4d12bccd45d4c2ad9913316c3dce3e533a007ba
Youssef-Hassane <youssef_hassane@icloud.com>
2024-03-11 03:21:47 +0200

    added console.py

--- console.py
+++ console.py
@@ -167,5 +167,170 @@ class HBNBCommand(cmd.Cmd):
             new_list = [str(obj) for key, obj in storage.all().items()]
             print(new_list)
 
+    def do_update(self, arg):
+        """
+        Update attribute of an instance
+        """
+        # split arguments
+        args = arg.split()
+
+        # if class name is missing, print error and return False
+        if len(args) < 1:
+            print("** class name missing **")
+            return False
+
+        # if class is not in classes list, print error and return False
+        className = args[0]
+        if className not in HBNBCommand.classes:
+            print("** class doesn't exist **")
+            return False
+
+        # if instance id is missing, print error and return False
+        if len(args) < 2:
+            print("** instance id missing **")
+            return False
+
+        # retrieve all objects and get object key
+        allObjects = storage.all()
+        objectKey = "{}.{}".format(className, args[1])
+
+        # if object is not found, print error and return False
+        if objectKey not in allObjects:
+            print("** no instance found **")
+            return False
+
+        # if attribute name is missing, print error and return False
+        if len(args) < 3:
+            print("** attribute name missing **")
+            return False
+
+        # if value is missing, print error and return False
+        if len(args) < 4:
+            print("** value missing **")
+            return False
+
+        # update attribute and save object
+        obj = allObjects[objectKey]
+        attrName = args[2]
+        attrValue = json.loads(args[3].replace("'", '"'))
+        obj.__dict__[attrName] = attrValue
+        obj.save()
+
+    def do_count(self, arg):
+        """
+        Retrieves the number of instances of a class
+        """
+        # Check if class name is provided
+        if not arg:
+            print("** class name missing **")
+            return
+
+        # Split the class name from the arguments
+        class_name = arg.split()[0]
+
+        # Check if class exists
+        if class_name not in HBNBCommand.classes:
+            print("** class doesn't exist **")
+            return
+
+        # Count the number of instances by iterating over all objects
+        # and counting those with the same class name
+        count = sum(
+            1
+            for obj in storage.all().values()
+            # Check if object's class name matches the class name
+            if type(obj).__name__ == class_name
+        )
+        # Print the count
+        print(count)
+
+#    def precmd(self, user_input):
+#     """Pre-command hook to handle special commands
+#     Args:
+#         user_input (str): input from user
+#     Returns:
+#         str: modified input if special command,
+#         otherwise original input
+#     """
+#     if not isinstance(user_input, str):
+#         return user_input
+#     input = regularExpression.match("(.*)[.](.*)[(](.*)[)]", user_input)
+#     if not input:
+#         # Return original input if no special command
+#         return user_input
+#     else:
+#         class_name = input.group(1)
+#         command_name = input.group(2)
+#         args = input.group(3)
+#         # Handle dictionary arguments
+#         if ("{" in args) and ("}" in args):
+#             args = \
+#                 args.replace(", ", " $from_dict$ ", 1).replace('"', "", 2)
+#         else:
+#             args = args.replace(", ", " ").replace('"', "", 2)
+#         # Modify and return input
+#         return "{} {} {}".format(command_name, class_name, args)
+
+    def default(self, line):
+        """
+        Parse the line and execute the corresponding method
+
+        Args:
+            line (str): input line containing the method, class and args
+
+        Returns:
+            bool: True if the method was executed successfully, False otherwise
+        """
+        # Map of methods
+        method_map = {
+            "all": self.do_all, "show": self.do_show,
+            "destroy": self.do_destroy, "count": self.do_count,
+            "update": self.do_update
+        }
+        # Parse the line to extract method, class and args
+
+        # Match the line with the given regular expression
+        matches = regularExpression.findall(r"(.*)\.(.*)\((.*)\)", line)
+
+        # If the line matches and has at least 2 groups
+        if matches and len(matches[0]) >= 2:
+            class_name, method_name, *args_str = matches[0]
+
+            # If the method is "update" and
+            # args_str is not empty and ends with '}'
+            if (method_name == "update" and args_str and
+                    args_str[0] and args_str[0][-1] == '}'):
+                # Split args_str by comma and get
+                # the updated_id and updated_dict_str
+                id_dict_list = args_str[0].split(',', 1)
+                updated_id, updated_dict_str = id_dict_list
+
+                # Load the json string into a dictionary
+                # and update the attributes
+                updated_dict = json.loads(updated_dict_str.replace("'", '"'))
+                for key, value in updated_dict.items():
+                    if isinstance(value, str):
+                        value = f'"{value}"'
+                    # Format the argument and execute the update method
+                    arg = f"{class_name} {updated_id} {key} {value}"
+                    if self.do_update(arg) is False:
+                        break
+                return
+            # If args_str is not empty, split it by comma and get args
+            elif args_str:
+                args = ','.join(args_str).split(',')
+            else:
+                args = []
+            # Join the class_name and args with space and strip the space
+            # Execute the corresponding method and return its result
+            arg = " ".join([class_name, *args]).rstrip(' ')
+            return method_map[method_name](arg)
+
+        # If the line doesn't match the regular
+        # expression, print an error message
+        print("*** Unknown syntax: {}".format(line))
+        return False
+
+
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
